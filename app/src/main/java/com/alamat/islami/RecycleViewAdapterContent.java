9f350cd53590ddabc15097c317e3c8206e2b5a57
Ruba <rubadel98@gmail.com>
2021-07-04 02:56:22 +0300

    الفلسفة تصنع المشاكل. i fixed the problems

---
+++ app/src/main/java/com/alamat/islami/RecycleViewAdapterContent.java
@@ -0,0 +1,56 @@
+package com.alamat.islami;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.alamat.islami.databinding.ItemContentBinding;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+
+public class RecycleViewAdapterContent extends RecyclerView.Adapter<RecycleViewAdapterContent.ViewHolder>  {
+
+    ArrayList<String> contentLines;
+
+    public RecycleViewAdapterContent(ArrayList<String> contentLines) {
+        this.contentLines = contentLines;
+    }
+
+    @NonNull
+    @NotNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        ItemContentBinding binding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()), R.layout.item_content,parent,false);
+        return new ViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.itemContentBinding.tvContent.setText(contentLines.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        if(contentLines == null){
+            return 0;
+        }
+        else {
+            return contentLines.size();
+        }
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder{
+        ItemContentBinding itemContentBinding;
+        public ViewHolder(ItemContentBinding itemContentBinding) {
+            super(itemContentBinding.getRoot());
+            this.itemContentBinding = itemContentBinding;
+        }
+    }
+
+}
