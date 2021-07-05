41c9f1bc39fa694c2acedfd81ae1a1602bad163c
Ruba <rubadel98@gmail.com>
2021-07-05 09:08:46 +0300

    -

---
+++ app/src/main/java/com/alamat/islami/SplashScreen.java
@@ -0,0 +1,17 @@
+package com.alamat.islami;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+
+        startActivity(new Intent(this,MainActivity.class));
+    }
+}
